3119b73f2a8c8c7cb4b1fc3c53492edc03649c1f
Simon Taylor <38040141+WhiskyDickens@users.noreply.github.com>
2021-09-25 22:18:25 +0100

    html constructor for displaying built forms

--- assets/js/form_constructor.js
+++ assets/js/form_constructor.js
@@ -1,58 +1,75 @@
 // Construct a form layout
-export function formConstructor(formData, $) {
+export function formConstructor(form, $) {
   // Grab form data
-  const groups = formData.groups;
-  const fields = formData.fields;
+  const groups = form.groups;
+  const fields = form.fields;
+
   // Start creating the HTML
-  let html = '<div class="row">';
-  let inlineScript = "<script>jQuery(document).ready(function($){/*";
+  const endpoint = form.custom_endpoint ? form.custom_endpoint : `./langs/${appVersion}/${ajaxEndpoints[appVersion]["test"]}`;
+  let html = `<form id='${form.form_id}' class='test-form' action='${endpoint}' action='post'><div class="row">`;
+  let inlineScript = `<script>jQuery(document).ready(function($){`;
+
   // Loop through groups
   $.each(groups, function (group_id, group) {
-    html += `<div id='${group_id}' class='col-md-${group.width} order-${group.order}'>`;
+    const custom_classes = group.custom_classes ? ` ${group.custom_classes}` : "";
+    html += `<div id='${group_id}' class='col-md-${group.width} order-${group.order}${custom_classes}'>`;
     // Loop through fields
     $.each(group.fields, function (key, field_id) {
       html += createField(field_id, fields[field_id]);
-      inlineScript += fieldScript(field_id, fields[field_id]);
+      inlineScript += fieldScript(field_id, fields[field_id]["script"]);
     });
     html += "</div>";
   });
-  inlineScript += "*/});</script>";
-  html += "</div>";
+
+  inlineScript += "});</script>";
+  html += `</div><div class='response'></div><script src='${appRoot}assets/js/form.js'></script></form>`;
+
   // Return the HTML
-  const jsonData = JSON.stringify(formData, null, 2);
-  return `${html + inlineScript}<hr><code><pre>${jsonData}</pre></code>`;
+  const jsonData = JSON.stringify(form, null, 2);
+  return `${html + inlineScript}<hr><p><strong>Form JSON:</strong></p><code><pre>${jsonData}</pre></code>`;
 
   // Create a single form field
-  function createField(field_id, field_data) {
-    let html = "";
-    const fieldTag = field_data.tag;
-    const value = field_data.value ? ` value='${field_data.value}'` : "";
-    const placeholder = field_data.placeholder ? ` placeholder='${field_data.placeholder}'` : "";
+  function createField(field_id, field) {
+    let html,
+      classes,
+      messages = "";
+
+    // Create HTML elements based on data
+    const fieldTag = field.tag;
+    const placeholder = field.placeholder ? ` placeholder='${field.placeholder}'` : "";
     const name = ` name='${field_id}'`;
-    const required = field_data.required ? " required" : "";
-    const classes = ` class='form-control${field_data.custom_classes ? ` ${field_data.custom_classes}` : ""}'`;
-    const help = field_data.help ? `<span data-bs-toggle="tooltip" data-bs-placement="top" title='${field_data.help}'><i class='fas fa-question-circle ms-1'></i></span>` : "";
-    const label = field_data.label ? `<label for='${field_id}'>${field_data.label}${required ? ` <small>*</small>` : ``}${help ? help : ``}</label>` : "";
+    const required = field.required ? " required" : "";
+    const help = field.help ? `<span data-bs-toggle='tooltip' data-bs-placement='${field.tooltip_position}' title='${field.help}'><i class='fas fa-question-circle ms-1'></i></span>` : "";
+    const label = field.label ? `<label for='${field_id}' class='form-label'>${field.label}${required ? ` <small>*</small>` : ``}${help ? help : ``}</label>` : "";
+    const size = field.size ? `${field.size}` : "";
+    const color = field.color ? `${field.color}` : "";
+    const value = field.value ? ` value='${field.value}'` : "";
+    const pattern = field.pattern ? ` data-pattern='${field.pattern}'` : "";
+    // If present, add data messages
+    if (field.messages) {
+      $.each(field.messages, function (check, message) {
+        messages = ` data-${check} = "${message}"`;
+      });
+    }
+
     switch (fieldTag) {
+      // Input fields
       case "input":
-        html += `${label}<input type='${field_data.type}'${classes}${name}${placeholder}${value}${required}>`;
+        classes = ` class='form-control${size ? ` input-${size}` : ""}${field.custom_classes ? ` ${field.custom_classes}` : ""}'`;
+        html = `${label}<input type='${field.type}'${classes}${name}${placeholder}${value}${messages}${pattern}${required}>`;
+        break;
+      // Buttons
+      case "button":
+        classes = ` class='btn btn-${size} btn-${color}${field.custom_classes ? ` ${field.custom_classes}` : ""}'`;
+        html = `${label}<button type='${field.type}'${classes}${name}>${field.value}</button>`;
         break;
     }
+
     return html;
   }
 
   // Add field's script to form's inline script
-  function fieldScript(field_id, field_data) {
-    return `
-    "format": "(.*)@(.*).(.*)",
-    "validation": {
-      "empty": "You must enter an email address",
-      "format": "Please enter a valid email address"
-    },
-    "requirements": {
-      "fields": false,
-      "groups": false
-    },
-    "unavailable": "hide"`;
+  function fieldScript(field_id, field_script) {
+    return "";
   }
 }
