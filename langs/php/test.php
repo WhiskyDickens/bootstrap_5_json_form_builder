186353dda58b4a3b651bd6441b4056eb1c001932
Simon Taylor <38040141+WhiskyDickens@users.noreply.github.com>
2021-09-26 12:18:33 +0100

    added test submission logging

--- langs/php/test.php
+++ langs/php/test.php
@@ -1,3 +1,30 @@
 <?php
-  echo json_encode($_POST, JSON_PRETTY_PRINT);
+  
+  require_once('config.php');
+  
+  // Grab debug info for logging
+  $debug = json_decode($_POST['debug'], true);
+  $timestamp = time();
+
+  // Remove debug from post for response
+  unset($_POST['debug']);
+
+  // Generate log
+  $log_array = [
+    "timestamp" => date('Y-m-d h:m:s', $timestamp),
+    "form" => $debug['form'],
+    "user" => $debug['username'],
+    "form_data" => $_POST
+  ];
+  $log = json_encode($log_array)."\n";
+
+  // Write response to log file
+  $filename = date('Y-m-d', $timestamp) ."-tests.txt";
+  $log_file = fopen(APP_PATH . "logs/" . $filename, "a");
+  fwrite($log_file, $log);
+  fclose($log_file);
+
+  // Return json array
+  echo json_encode($log_array, JSON_PRETTY_PRINT);
+
 ?>
--- langs/python/test.py
+++ langs/python/test.py
@@ -1,24 +1,49 @@
 #!C:/Python39/python.exe
 
 # Import modules
+import config
 import sys
 import json
 import cgi
+from datetime import datetime
 
 # Set defaults
 field = ""
 value = ""
+log_dict = {}
 response = {}
 
 # Grab form data
 form = cgi.FieldStorage()
+
 # Concat each form field name and value to string
 for key in form.keys():
         field = str(key)
         value = str(form.getvalue(field))
         response[field] = value
 
+# Generate log
+debug = json.loads(response["debug"])
+now = datetime.now()
+
+# Remove debug from post for response
+response.pop("debug")
+
+#Generate log
+log_dict["timestamp"] = now.strftime("%Y-%m-%d %H:%M:%S")
+log_dict["form"] = debug["form"]
+log_dict["user"] = debug["username"]
+log_dict["form_data"] = response
+log = json.dumps(log_dict) +"\n";
+
+# Write response to log file
+filename = now.strftime("%Y-%m-%d")
+filename += "-tests.txt";
+log_file = open(config.app_root +"logs/"+ filename, "a");
+log_file.write(log);
+log_file.close();
+
 sys.stdout.write("Content-Type: application/json")
 sys.stdout.write("Status: 200 OK")
 sys.stdout.write("\n\n")
-sys.stdout.write(json.dumps(response))
+sys.stdout.write(json.dumps(log_dict, indent=2))
