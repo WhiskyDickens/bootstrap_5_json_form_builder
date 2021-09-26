8caba0906d1f19a6483dbc69e1ff9b0f4f7d0b59
Simon Taylor <38040141+WhiskyDickens@users.noreply.github.com>
2021-09-26 12:41:51 +0100

    update cors policy for github.io requests

--- langs/php/config.php
+++ langs/php/config.php
@@ -3,4 +3,12 @@
    * Global configuration file
    */
 
+  // Define app root directory
   define('APP_PATH', dirname(dirname(__DIR__)) ."/");
+
+  // CORS: Allow requests from github pages
+  if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] == 'https://whiskydickens.github.io') {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+  }
