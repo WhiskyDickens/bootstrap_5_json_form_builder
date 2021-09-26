08682ec82fbab7f07ae9fbe01cf4341a01d57ca3
Simon Taylor <38040141+WhiskyDickens@users.noreply.github.com>
2021-09-26 12:46:52 +0100

    add CORS policy to .htaccess, remove from php

--- langs/php/config.php
+++ langs/php/config.php
@@ -5,10 +5,3 @@
 
   // Define app root directory
   define('APP_PATH', dirname(dirname(__DIR__)) ."/");
-
-  // CORS: Allow requests from github pages
-  if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] == 'https://whiskydickens.github.io') {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-  }
